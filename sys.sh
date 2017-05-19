86004b71f57c3bd8949cf7cdfe74f55a112054ee
Oumar Djimé Ratou <oudjira90@gmail.com>
2017-05-19 23:18:01 +0200

    Add files via upload

--- sys.sh
+++ sys.sh
@@ -1,36 +1,69 @@
 #!/bin/bash
+#fonction qui permet de liste les utilisateur integre il y'a 3 jour
+	function showUsers(){
+	 ### on détermine d'abord la liste des différents utilisateurs enregistrés sachant qu'ils ont un dossier personnel à leur nom dans /home/
+	   cat /etc/passwd | cut -d ":" -f1 > users.txt
+	   cat users.txt | while read ligne
+	 do
+	   ls /home/ | grep $ligne > users.txt
+	 done
+	 echo "la liste des utilisateurs est : "
+	 while read ligne
+	 do
+	  echo "- $ligne"
+	 done < users.txt
+	 ##### ici il doit être question de filtrer les utilisateurs des 03 derniers jours
+	}
 
-while true
-clear
+#fonction qui permet d'archiver les element du repertoire edit par sudoer
+	function archive(){
+           read -p "entrer le chemin du périphérique externe : " path
+	   user=$(cat /etc/group | grep sudo | cut -d ":" -f4)
+	   sudo find /home/$user/* -ctime 2 > fichierstock.txt #on stocke tout les fichiers modifiés il y a deux jours dans le fichier fichierstock.txt
+	   mkdir archive
+           cat fichierstock.txt | while read ligne
+	 do
+	   mv $ligne archive
+	done
+	  tar -cvs archive.tar archive/
+	  gzip archive.tar	
+	  cp archive.tar.gz $path
+	}
+
+
+question=1
+
+while [ "$question" -eq 1 ];
 do
-#mot de bien venue
-
-echo    "	================================================================
-	*								*
-	*   BIENVENUE SUR MON PROJET SYS SAISIR L'ATION À EXÉCUTER      *
-        *								*
-        *===============================================================*"
-
+ clear
 #menu qui nous permet d'exécuter notre projet sys
+ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+ echo "*                                                                                            *"
+ echo "*                              BIENVENU DANS MON PROJET SYS                                  *"
+ echo "**********************************************************************************************"
+ echo "*                                                                                            *"
+ echo "* a) Informations des utilisateurs enregistré il y'a trois (03) jours                        *"
+ echo "* b) Aquisition, Installation et Lancement de l'environnement XAMPP                          *"
+ echo "* c) Archivage des éléments du repertoire personnel qui ont été modifier par l'utilisateur   *" 
+ echo "*    sudoer il y'a deux jours dans le peripherique externe                                   *"
+ echo "* d) Information sur l'utilisation du disque, de la mémoire, du processeur et du swap        *"
+ echo "* q) Quitter !                                                                               *"
+ echo "*											    *"
+ echo "*                                                                                            *"
+ echo "*                     RÉALISER PAR OUMAR DJIME RATOU 14B486FS                                *"
+ echo "*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*"
 
-	echo " a) Informations des utilisateurs enregistré il y'a trois (03) jours"
-	echo " b) Aquisition, Installation et Lancement de l'environnement XAMPP "
-	echo " c) Archivage des éléments du repertoire personnel qui ont été modifier par l'utilisateur sudoer il y'a deux jour dans le peripherique eterne"
-	echo " d) Information sur l'utilisation du disque, de la mémoire, du processeur et du swap"
-	echo " q) Quitter !!! "
 
 	echo -e "\n"
 	echo -e "Entrer votre selction : \c "
 	read reponse
 
 	case "$reponse" in 
-		a) who ;; 
+		a) showUsers ;; 
 		#prémiere commande "weget" permet de recupere le fichier executable depuis le site officiel xampp
 		#deuxième commande "sudo chmod xampp..." permet de donner tous les droit au users et executer et lecture pour les autres
 		#toisieme commande "sudo ./xamp......." permet d'executer le programme
 		#quatrième commande "sudo /opt/lampp/lampp start" permet de demarer xampp
-		
-		#Permet de verifier si XAMPP est installé si ce le cas il le demarre sinon il le telecharger et install  et s'il y'a error il le rdirige vers le fichier errtest
 		b)sudo /opt/lampp/lampp start 2> errtest ;
 		  echo "XAMPP est déjà installé ";
 			if [ $? -ne 0 ]; then
@@ -39,7 +72,8 @@ echo    "	================================================================
 				sudo ./xampp-linux-*-installer.run;
 				sudo /opt/lampp/lampp start 2> errtest;
 			fi ;;
-		c) find -ctime 3;;
+		#appel de la fonction archive();
+		c) archive;;
 		#la commande vmstat permet de donner les informations sur la memoire, cpu, disque dur et swap
 		d)vmstat ;;
 		#permt de quit le processus
